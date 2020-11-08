0e347aa46850bf8dd9a509142bb391db1dc450b0
joon <kweonhj1542@gmail.com>
2020-11-08 14:30:15 +0900

    8th of Nov

---
+++ JavaScript/Part3.js
@@ -0,0 +1,9 @@
+var lbs = prompt("What is the weight in pounds (lbs)?")
+var kg = lbs*0.454
+alert("That is:  "+kg+" kilograms")
+console.log("Conversion Completed")
+
+var lbs = prmopt("Enter you weight")
+var kg = lbs * 0.45;
+alert("That is " + kg + " kilograms");
+console.log("your such fatt dude")
